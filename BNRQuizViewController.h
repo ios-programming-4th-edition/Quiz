0dcbab0a7a7486ea574313c4f88c7d4651b1576a
Richard Millet <remillet@yahoo.com>
2014-02-26 22:30:29 -0800

    Added xib file with two labels and two buttons.

---
+++ BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Richard E Millet on 2/26/14.
+//  Copyright (c) 2014 remillet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
